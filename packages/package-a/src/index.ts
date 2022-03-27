699f5cb58ada57fa29a9dd10f911c456ea870296
xiaoYown <qzy09101018@sina.com>
2022-03-27 17:42:15 +0800

    fix(@xv-publish-test/changeset-test-a): Fix

--- packages/package-a/src/index.ts
+++ packages/package-a/src/index.ts
@@ -1,6 +1,10 @@
 import packageB from '@xv-publish-test/changeset-test-b';
 import packageC from '@xv-publish-test/changeset-test-c';
 
+export const exec01 = () => {
+  console.log('fix');
+};
+
 function templateEg(): void {
   console.log('Lib exempli.');
   console.log(packageB);
